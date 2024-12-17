5641c3c266674b9dd41baf48906b25c489e47cc3
Mohit Sharma <mohitsharma55562005@gmail.com>
2024-12-17 22:35:06 +0530

    Add files via upload
    Features
    💧 Custom Reminders: Set personalized water intake reminders at desired intervals.
    📊 Hydration Tracking: Log your water consumption and track progress.
    🎯 Daily Goals: Set daily water intake goals based on your activity level and weight.
    📱 User-Friendly Interface: Simple and easy-to-use design for seamless tracking.
    🔔 Notifications: Get timely notifications to remind you to drink water.

---
+++ drink.py
@@ -0,0 +1,10 @@
+from plyer import notification
+import time
+
+
+notification.notify(
+    title = "It's time to drink the fucking water",
+    message = "You haven't dranl water since the morning so pls get the shitty ass out of the chair and dink atlast 2 glass water",
+    app_icon = r"D:\Python related\Pythonnn\Waterdrinking reminder\drink_119593.ico",
+    timeout = 10
+)
